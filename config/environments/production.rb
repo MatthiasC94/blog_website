b986a900050ed6d9fa3fd6c46ed1411c0b9d4660
MatthiasC94 <matthias.cordes@me.com>
2016-07-14 01:51:56 +0200

    höu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-config.serve_static_files = true
-config.assets.digest = true
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -26,7 +23,7 @@ config.assets.digest = true
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
