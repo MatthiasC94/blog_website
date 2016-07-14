b3a2ee36d9c0fb1a6bd409a0ddb279bf09c2d273
MatthiasC94 <matthias.cordes@me.com>
2016-07-14 12:53:15 +0200

    uhil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714094553) do
+ActiveRecord::Schema.define(version: 20160714104703) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160714094553) do
     t.string   "links"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
 
 end
