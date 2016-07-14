a3f20faf1f00669b0ba3c5246f536d634cc0524d
MatthiasC94 <matthias.cordes@me.com>
2016-07-14 14:59:52 +0200

    hiu

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,8 +18,27 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
+	end
+
+	def edit
 
 	end
+
+	def update
+		if @project.update project_params
+		redirect_to @project, notice: "successfully saved!"
+		else
+		render 'edit'
+		end
+	end
+
+	def destroy 
+		@project.destroy
+		redirect_to projects_path
+	end
+
+
+
 
 	private
 
@@ -31,3 +50,4 @@ class ProjectsController < ApplicationController
 		params.require(:project).permit(:title, :description, :slug)
 	end
 end
+
