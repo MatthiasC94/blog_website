c2083312e6af7cf6b9aa7b9a6e2d5f82bdc22f07
MatthiasC94 <matthias.cordes@me.com>
2016-07-15 15:38:27 +0200

    login

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'redcarpet', '~> 3.3', '>= 3.3.4'
 gem 'friendly_id', '~> 5.1'
 gem 'will_paginate', '~> 3.1.0'
 
+gem 'devise', '~> 4.2'
+
 group :development, :test do
   gem 'byebug', platform: :mri
 end
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,8 @@
 require_relative 'boot'
-
 require 'rails/all'
+	Bundler.require(*Rails.groups)
 
-
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-
-module Blog
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
+	module Blog
+  	class Application < Rails::Application
+  	end
 end
