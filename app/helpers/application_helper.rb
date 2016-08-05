bb2f94a41e29b6105a9e735dcb90e84faef9061a
MatthiasC94 <matthias.cordes@me.com>
2016-08-05 14:14:14 +0200

    öuh

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,41 @@ module ApplicationHelper
 		def block_code(code, language)
 			Pygments.highlight(code, lexer: language)
 		end
+
+	    def autolink(link, link_type)
+	      case link_type
+	      when :url then url_link(link)
+	      end
+	    end
+
+	    def url_link(link)
+	      if link.include?("https://www.youtube.com")
+	        youtube_link(link)
+
+	       elsif link.include?("https://www.facebook.com/")
+	        facebook_link(link)
+
+	      else
+	        normal_link(link)
+	      end
+	    end
+
+	    def youtube_link(link)
+	      parameters_start = link.index('=')
+	      video_id = link[parameters_start + 1..-1]
+	      "<iframe width=\"100%\" height=\"400\" src=\"https://www.youtube.com/embed/#{video_id}?autoplay=0\" allowfullscreen></iframe>"
+	    end
+
+	    def facebook_link (link)
+	      parameters_start = link.index('videos/')
+	      video_id = link[parameters_start + 1..-1]
+	      "<iframe width=\"100%\" height=\"400\" src=\"https://www.facebook.com/video.php?v={id}\" allowfullscreen></iframe>"
+	    end
+
+	    def normal_link(link)
+	      "<a href=\"#{link}\" target=\"_blank\">#{link}</a>"
+	    end
+
 	end
 	def markdown(content)
 		renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
