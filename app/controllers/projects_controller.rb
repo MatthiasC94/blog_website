fd625f312194b30839025e77a4cc7410696bd543
MatthiasC94 <matthias.cordes@me.com>
2016-08-07 13:33:53 +0200

    lhkj

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
 
 
 	def index 
-		@projects = Project.all.order("created_at desc")
+		@projects = Project.all.order("created_at desc").paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def new
