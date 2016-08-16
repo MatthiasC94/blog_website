7206893c77498470bc349bbe533997cf7e901c2a
MatthiasC94 <matthias.cordes@me.com>
2016-08-16 17:44:05 +0200

    “blabla”

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,25 @@ class WelcomeController < ApplicationController
 	def index
 		@posts = Post.all.order("created_at desc")
 	end
+before_action :find_project, only: [:show, :edit, :update, :destroy]
+		before_action :authenticate_usermodel!, except: [:index, :show]
+
+
+	def index 
+		@projects = Project.all.order("created_at desc").paginate(:page => params[:page], :per_page => 3)
+	end
+
+	def show
+	end
+
+	private
+
+	def find_project
+		@project = Project.friendly.find(params[:id])
+	end
+
+	def project_params
+		params.require(:project).permit(:title, :description, :slug, :image)
+	end
+
 end
