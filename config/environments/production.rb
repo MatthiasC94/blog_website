6351f9a543a115bfcdfe17ffed1e9fcf1f9d5244
MatthiasC94 <matthias.cordes@me.com>
2017-02-10 21:18:51 +0100

    “blabla”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "blog_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  #config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -97,6 +97,23 @@ config.paperclip_defaults = {
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
   }
 }
+
+config.action_mailer.default_url_options = { host: 'https://pacific-forest-9589.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
+
+
+
 
 
 end
